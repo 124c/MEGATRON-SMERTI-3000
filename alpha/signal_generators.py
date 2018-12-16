0972f062d43a9cb0dc01ca2c603e6bace217b82c
124c <mikayil.majidov@gmail.com>
2018-12-16 16:52:49 +0300

    moving to pre-built indicators. Remaining problem with hurst, adding signal generator for rsi.

--- alpha/signal_generators.py
+++ alpha/signal_generators.py
@@ -15,10 +15,44 @@ def momentum_signals(momentum_values):
     return momentum_signs
 
 
-def macd_signals(macd_dataframe, upper_threshold=0.00015, lower_threshold=-0.00015):
-    """Generates buy/sell signals with mean reversion logic"""
-    sigline = macd_dataframe['MACD_signal_line']
+def macd_signals(signal_line, upper_threshold=0.000, lower_threshold=-0.000):  # 15
+    """
+    Generates buy/sell signals with macd logic
+    """
+    sigline = signal_line
     macd_signs = np.where(sigline > upper_threshold, -1,
                           np.where(sigline < lower_threshold, 1, 0))
-    macd_signs = pd.Series(macd_signs, index=macd_dataframe.index)
+    macd_signs = pd.Series(macd_signs, index=signal_line.index)
     return macd_signs
+
+
+def rsi_signals(signal_line, upper_threshold=70, lower_threshold=30):  # 15
+    """
+    Generates buy/sell signals with mean reversion logic
+    """
+    sigline = signal_line
+    rsi_signs = np.where(sigline > upper_threshold, -1,
+                         np.where(sigline < lower_threshold, 1, 0))
+    rsi_signs = pd.Series(rsi_signs, index=signal_line.index)
+    return rsi_signs
+
+
+# TODO: Smb should take it as an assignment
+def generate_signal_dataframe(indicators_df):
+    """
+    Accepts a dataframe with indicator values as input and orienting by colnames,
+    generates a dataframe with signals
+    """
+    return 'not ready'
+
+
+def hurst_filter(momentum_ind, meanreverse_ind, hurst, mom_barrier=0, meanrev_barrier=0):
+    if mom_barrier < meanrev_barrier:
+        raise ValueError('Your threshold values overlap!\n Please change your threshold values')
+
+    mom_signals = np.where(hurst.values > mom_barrier, momentum_ind, 0)
+    meanreversion_signals = np.where(hurst < meanrev_barrier, meanreverse_ind, 0)
+    signals = meanreversion_signals + mom_signals
+    print(signals.shape)
+    signals = pd.DataFrame(signals, index=momentum_ind.index)
+    return signals
---
+++ deploy.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from numpy.random import randn
+import talib
+
+from datasets.clean_dataset import load_ohlc_dataset
+import alpha.technical_indicators as tech
+import alpha.signal_generators as sign
+import backtesting.backtester as bt
+
+start_time = '2002-04-08'
+end_time = '2003-04-07'
+
+data = load_ohlc_dataset(start_time=start_time, end_time=end_time)
+print('check for Na in datasets: \n', data.isnull().any())
+# 04 09 12 00
+
+indicator_data = pd.DataFrame()
+hurst_data = tech.hurst_exponent(data['close'], 24)
+# tech.hurst_core(data['close'][133:143])
+macd, macdsignal, macdhist = talib.MACD(data['close'].shift(), fastperiod=5, slowperiod=8, signalperiod=4)
+rsi_values = talib.RSI(data['close'].shift(), timeperiod=5)
+
+indicator_signals = pd.DataFrame()
+indicator_signals['macd_signal'] = sign.momentum_signals(macdsignal)
+indicator_signals['rsi_signal'] = sign.rsi_signals(rsi_values)
+indicator_signals['hurst'] = hurst_data
+indicator_signals['filtered_signals'] = sign.hurst_filter(momentum_ind=indicator_signals['macd_signal'],
+                                                          meanreverse_ind=indicator_signals['rsi_signal'],
+                                                          hurst=indicator_signals['hurst'],
+                                                          mom_barrier=0,
+                                                          meanrev_barrier=0)
+
+pnl_data = bt.get_profit_and_loss(data=data['close'].pct_change(), signals=indicator_signals)
+plt.plot(pnl_data['filtered_signals_pnl'].cumsum())
+plt.plot(pnl_data['momentum_signal_pnl'].cumsum())
+
+
+

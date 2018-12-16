bccea4ebb5ec3c857dc72596433e76029935594a
124c <mikayil.majidov@gmail.com>
2018-12-17 00:39:27 +0300

    adding test year which is next (50% training-50%test)

--- deploy.py
+++ deploy.py
@@ -73,6 +73,35 @@ print(sum(pnl_data['filtered_signals_pnl'].dropna()))
 plt.plot(pnl_data['filtered_signals_pnl'].cumsum())
 plt.plot(pnl_data['rsi_signal_pnl'].cumsum())
 
+# now check for the next year
+start_time = '2003-04-08'
+end_time = '2004-04-08'
+data = load_ohlc_dataset(start_time=start_time, end_time=end_time)
+print('check for Na in datasets: \n', data.isnull().any())
 
+indicator_data = pd.DataFrame()
+hurst_data = tech.hurst_exponent(data['close'], 24)
+# tech.hurst_core(data['close'][133:143])
+macd, macdsignal, macdhist = talib.MACD(data['close'].shift(), fastperiod=5, slowperiod=8, signalperiod=4)
+rsi_values = talib.RSI(data['close'].shift(), timeperiod=rsi_period)
+
+indicator_signals = pd.DataFrame()
+indicator_signals['macd_signal'] = sign.momentum_signals(macdsignal)
+indicator_signals['rsi_signal'] = sign.rsi_signals(rsi_values, upper_threshold=rsi_upper, lower_threshold=rsi_lower)
+indicator_signals['hurst'] = hurst_data
+indicator_signals['filtered_signals'] = sign.hurst_filter(momentum_ind=indicator_signals['macd_signal'],
+                                                          meanreverse_ind=indicator_signals['rsi_signal'],
+                                                          hurst=indicator_signals['hurst'],
+                                                          mom_barrier=0.1,
+                                                          meanrev_barrier=-0.1)
+
+pnl_data = bt.get_profit_and_loss(data=data['close'].pct_change(), signals=indicator_signals, ex=1)
+conf_matrix = bt.get_confusion_matrix(data['close'], pnl_data['filtered_signals'])
+hit_ratio = bt.get_hit_ratio(conf_matrix)
+print(hit_ratio)
+print(sum(pnl_data['filtered_signals_pnl'].dropna()))
+
+plt.plot(pnl_data['filtered_signals_pnl'].cumsum())
+plt.plot(pnl_data['rsi_signal_pnl'].cumsum())
 
 
